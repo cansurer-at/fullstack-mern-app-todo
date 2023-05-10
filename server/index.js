a2408d5300c514e34e3bfead8882bcb62715c6e3
Can Surer <cansurer@outlook.com>
2023-05-11 00:20:51 +0200

    first route and connection succes

--- server/index.js
+++ server/index.js
@@ -8,13 +8,16 @@ app.use(express.json());
 //Port
 const PORT = process.env.PORT || 5500;
 
+//import routes
+const TodoItemRoute = require("./routes/todoItems");
+
 //connect to mongodb ..
 mongoose
   .connect(process.env.DB_CONNECT)
   .then(() => console.log("Database connected"))
   .catch((err) => console.log(err));
 
-//import routes
+app.use("/", TodoItemRoute);
 
 //connect to server
 app.listen(PORT, () => console.log("Server connected"));
--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -1,5 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 //import todo model
 
-const todoItemsModel = require('../models/todoItems')
+const todoItemsModel = require("../models/todoItems");
+
+router.post("/api/item", async (req, res) => {
+  try {
+    const newItem = new todoItemsModel({
+      item: req.body.item,
+    });
+    //save this item in database we must use async - await when dealing with promises and database
+
+    const saveItem = await newItem.save();
+    res.status(200).json("Item Added Successfully");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+
+
+//export router
+module.exports = router 
