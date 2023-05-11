9bffa3068674a1c12f9ed029829ca2fa1800c6b1
Can Surer <cansurer@outlook.com>
2023-05-11 10:22:36 +0200

    backend server and rest api complete, fe initial c

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -12,7 +12,7 @@ router.post("/api/item", async (req, res) => {
     //save this item in database we must use async - await when dealing with promises and database
 
     const saveItem = await newItem.save();
-    res.status(200).json("Item Added Successfully");
+    res.status(200).json("Item Added Successfully.");
   } catch (err) {
     res.json(err);
   }
@@ -24,6 +24,33 @@ router.get("/api/items", async (req, res) => {
   try {
     const AllTodoItems = await todoItemsModel.find({});
     res.status(200).json(AllTodoItems);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//lets update item
+
+router.put("/api/item/:id", async (req, res) => {
+  try {
+    //find the item by its id and update it
+
+    const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json("Item Updated");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//lets delete item from database
+router.delete("/api/item/:id", async (req, res) => {
+  try {
+    //find the item by its id and update it
+
+    const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Item Deleted");
   } catch (err) {
     res.json(err);
   }
